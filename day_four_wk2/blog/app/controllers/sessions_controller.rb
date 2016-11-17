c7853190f28df689c4d5c22fa2733e73b99fd49f
Emre Surmeli <s.emre.s.8@gmail.com>
2016-11-17 15:45:02 -0500

    add re routing

--- day_four_wk2/blog/app/controllers/sessions_controller.rb
+++ day_four_wk2/blog/app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by_username( params[ :username ] )
+    # binding.pry
+    if @user && @user.authenticate( params[:password] )
+      session[ :user_id ] = @user.id
+      redirect_to '/users'
+    else
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    session[ :user_id ] = nil
+    redirect_to '/login'
   end
 end
--- day_four_wk2/blog/config/routes.rb
+++ day_four_wk2/blog/config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-    get 'sessions/new'
-
-    get 'sessions/create'
-
-    get 'sessions/destroy'
+    get  '/login'  => 'sessions#new'
+    post '/login'  => 'sessions#create'
+    get  '/logout' => 'sessions#destroy'
 
     resources :users
 
