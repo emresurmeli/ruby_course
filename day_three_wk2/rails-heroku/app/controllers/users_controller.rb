a9311536eb486df5fb70d648490fb4f28dce049f
Emre Surmeli <s.emre.s.8@gmail.com>
2016-11-17 10:35:38 -0500

    add all from Wednesday

--- day_three_wk2/rails-heroku/app/controllers/users_controller.rb
+++ day_three_wk2/rails-heroku/app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  # def show
-  #   @user = User.find( [ param: id] )
-  # end
+  def show
+    @user = User.find( params[ :id ] )
+  end
 end
